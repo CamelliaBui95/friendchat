815adb6e60677ec598049011f6c1a17981d8b0b2
CamelliaBui95 <camellia.bui95@gmail.com>
2024-01-16 16:25:05 +0100

    adjusting landing images

--- Client/friendchat/src/components/home/Home.jsx
+++ Client/friendchat/src/components/home/Home.jsx
@@ -9,12 +9,12 @@ const Home = () => {
             <h2 className='text-white text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
             <p className='text-grey-light text-md'>Hic repellendus deserunt a aspernatur cupiditate, voluptatibus dicta autem delectus iusto odit nisi quos accusantium earum, inventore magnam dignissimos eum repudiandae ipsa.</p>
         </Col>
-        <Col lg={5} className='relative'>
+        <Col lg={6} className='relative'>
             <img src="../images/bgDeco-right-flipped.png" alt="" width={535} height={628} className='absolute'/>
-            <img src="../images/bgDeco-left.png" alt="" width={487} height={628} className='absolute translate-x-70'/>
-            <img src="../images/male.png" alt="" />
-            <img src="../images/female.png" alt="" />
-            <img src="../images/bgDeco_2_square.png" alt="" />
+            <img src="../images/bgDeco-left.png" alt="" width={487} height={628} className='absolute translate-x-90'/>
+            <img src="../images/male.png" alt="" className='absolute top-minus-40 z-1'/>
+            <img src="../images/female.png" alt="" className='absolute z-1 top-minus-40 translate-x-70'/>
+            <img src="../images/bgDeco_2_square-flipped.png" alt="" width={900} className='absolute z-2 top-80 left-minus-5'/>
         </Col>
     </Row>
   )
